7263becbb6394ff144a5ea2b753f49025e2567cc
samzah <szahir@hotmail.com>
2016-04-29 03:56:48 -0400

    reforming

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var $veggies = $('.veggies');
+
+$veggies.waypoint(function (direction) {
+  if (direction == 'down') {
+    $veggies.addClass('js-veggies-animate');
+  } else {
+    $veggies.removeClass('js-veggies-animate');
+  }
+}, {offset: '50%'});
+
+$('div').on('click', function (e) {
+  $(this).toggleClass('is-clicked');
+});
+
